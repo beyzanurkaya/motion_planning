b03092d1e34aa8283e3b9aee64849990eaba88f2
beyza <beyzanurkya98@gmail.com>
2021-08-24 19:08:58 +0300

    update car

--- src/car.cpp
+++ src/car.cpp
@@ -4,76 +4,61 @@ using namespace std;
 using namespace chrono_literals;
 using placeholders::_1;
 
-class CarNode : public rclcpp::Node
-        {
-        public:
-            CarNode(): Node("car")
-            {
-                marker_publisher = this->create_publisher<visualization_msgs::msg::Marker>("car", 10);
-                poses_subscriber = this->create_subscription<geometry_msgs::msg::PoseArray>("sine_path" , 10, bind(&CarNode::topic_callback, this, _1));
+CarNode::CarNode(): Node("car")
+{
+    marker_publisher = this->create_publisher<visualization_msgs::msg::Marker>("car", 10);
+    poses_subscriber = this->create_subscription<geometry_msgs::msg::PoseArray>("sine_path" , 10, bind(&CarNode::topic_callback, this, _1));
 
-            }
+}
 
-        private:
+void CarNode::topic_callback(const geometry_msgs::msg::PoseArray::SharedPtr msg)
+{
 
-            void topic_callback(const geometry_msgs::msg::PoseArray::SharedPtr msg) const
-            {
+    markerD.header.frame_id = "map";
+    markerD.action = visualization_msgs::msg::Marker::DELETEALL;
 
-                int i = 0;
+    marker.header.frame_id = "map";
+    marker.ns = "my_namespace";
+    marker.id = 0;
+    marker.type = visualization_msgs::msg::Marker::CUBE;
+    marker.action = visualization_msgs::msg::Marker::ADD;
 
-                visualization_msgs::msg::Marker markerD;
-                markerD.header.frame_id = "map";
-                markerD.action = visualization_msgs::msg::Marker::DELETEALL;
+    marker.scale.x = 4.0;
+    marker.scale.y = 1.5;
+    marker.scale.z = 1.0;
 
-                visualization_msgs::msg::Marker marker;
-                marker.header.frame_id = "map";
-                marker.ns = "my_namespace";
-                marker.id = 0;
-                marker.type = visualization_msgs::msg::Marker::CUBE;
-                marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.color.r = 0.0f;
+    marker.color.g = 0.0f;
+    marker.color.b = 1.0f;
+    marker.color.a = 1.0;
 
-                marker.scale.x = 4.0;
-                marker.scale.y = 1.5;
-                marker.scale.z = 1.0;
+    for (int i = 0; i < 360; i++) {
 
-                marker.color.r = 0.0f;
-                marker.color.g = 0.0f;
-                marker.color.b = 1.0f;
-                marker.color.a = 1.0;
+        auto p = msg->poses.at(i);
+        double x = p.orientation.x;
+        double y = p.orientation.y;
+        double z = p.orientation.z;
+        double w = p.orientation.w;
+        double yaw = atan2(2.0f * (w * z + x * y), w * w + x * x - y * y - z * z);
 
+        double dx = cos(yaw)*2;
+        double dy = sin(yaw)*2;
 
-                do {
-                    auto p = msg->poses.at(i);
-                    double x = p.orientation.x;
-                    double y = p.orientation.y;
-                    double z = p.orientation.z;
-                    double w = p.orientation.w;
-                    double yaw = atan2(2.0f * (w * z + x * y), w * w + x * x - y * y - z * z);
+        p.position.x = dx + p.position.x;
+        p.position.y = dy + p.position.y;
 
-                    double dx = cos(yaw)*2;
-                    double dy = sin(yaw)*2;
+        if(i==0){
+            marker.pose = p;
+            marker.pose.position.y = 2.0;
+        }else
+            marker.pose = p;
 
-                    p.position.x = dx + p.position.x;
-                    p.position.y = dy + p.position.y;
+        marker_publisher->publish(marker);
+        rclcpp::sleep_for(1s);
+        marker_publisher->publish(markerD);
 
-                    if(i==0){
-                        marker.pose = p;
-                        marker.pose.position.y = 2.0;
-                    }else
-                        marker.pose = p;
-
-                    i++;
-                    marker_publisher->publish(marker);
-                    rclcpp::sleep_for(1s);
-                    marker_publisher->publish(markerD);
-                }while(i < 360);
-
-            }
-
-            rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_publisher;
-            rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr poses_subscriber;
-
-        };
+    }
+}
 
 int main(int argc, char * argv[])
 {
