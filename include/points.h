cc3e56b72596942a05f0d290deb1941c4577cbe8
beyza <beyzanurkya98@gmail.com>
2021-08-24 18:31:57 +0300

    update points header file for doxygen

--- include/points.h
+++ include/points.h
@@ -10,5 +10,71 @@
 #include "geometry_msgs/msg/pose.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
+using namespace std;
+using placeholders::_1;
+
+/*! This class contains a publisher named poses_publisher, a timer and a callback function named timer_callback() */
+
+class PointsNode : public rclcpp::Node
+        {
+        public:
+            PointsNode();
+
+        private:
+
+            /*!
+             *  @brief marker is a variable of type Marker that is a member of visualization_msgs.
+             *    - what's inside?
+             *         - frame_id
+             *         - ns
+             *         - id
+             *         - type
+             *         - action
+             *         - color
+             *         - scale
+             *         - pose
+             *            - position
+             *                - x,y,z
+             *            - orientation
+             *                - x,y,z,w
+             *    - Why is this variable used?
+             *         - this variable keeps positions came from sine_path node
+             */
+
+            visualization_msgs::msg::Marker marker;
+
+            /*!
+            *  @brief markerArray is a variable of type markerArray that is a member of visualization_msgs.
+            *    - what's inside?
+            *         - markers
+            *    - Why is this variable used?
+            *         - this variable keeps points of sine path
+            */
+
+            visualization_msgs::msg::MarkerArray markerArray;
+
+            void topic_callback(geometry_msgs::msg::PoseArray::SharedPtr msg);
+
+            /** @fn void PointsNode::topic_callback()
+             *  @brief The topic_callback function receives the message data published over the topic.
+             */
+
+            /**
+             *  @brief the publisher is a ROS node that publishes a specific type of ROS message over a given ROS topic. “Interested” nodes (Subscribers) can access messages so published.
+                  - what is published?
+             *         - markers_publisher publishes a markerArray with color, action, scale, positions and orientations specified.
+             */
+
+            rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_publisher;
+
+            /**
+              *  @brief the subscriber subscribes to the topic so that it receives the messages whenever any message is published to the topic.
+              *      - poses_subscriber gets position information for markers by subscribing to the PoseArray message of the sinus path node.
+              */
+
+            rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr poses_subscriber;
+
+        };
 
 #endif //BUILD_POINTS_H
+
