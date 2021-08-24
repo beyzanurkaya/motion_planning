0a953c4b0d96a2d71f4faafd4fe7c3920793f18d
beyza <beyzanurkya98@gmail.com>
2021-08-24 14:50:29 +0300

    update sine header

--- include/sine_path.h
+++ include/sine_path.h
@@ -13,5 +13,25 @@
 #include "Eigen/Dense"
 #include <Eigen/Geometry>
 
+using namespace std;
+using namespace chrono_literals;
+
+class SinePath : public rclcpp::Node
+        {
+        public:
+
+            SinePath();
+
+        private:
+            geometry_msgs::msg::PoseArray poseArray;
+            geometry_msgs::msg::Pose p;
+            Eigen::Quaternionf q;
+
+            void timer_callback();
+            rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr poses_publisher;
+            rclcpp::TimerBase::SharedPtr timer;
+
+        };
+
 #endif //BUILD_SINE_PATH_H
 
